53eca62bdafafc45da87c3984f7284d4e67acc7f
Ayush Saha <ayushsaha.work@gmail.com>
2025-07-27 15:26:57 +0530

    Added footer: Made with ❤️ by Ayush Saha

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="fixed bottom-2 right-4 text-sm text-gray-600 z-50">
+      Made with <span className="text-red-500">❤️</span> by <strong>Ayush Saha</strong>
+    </div>
+  );
+};
+
+export default Footer;
