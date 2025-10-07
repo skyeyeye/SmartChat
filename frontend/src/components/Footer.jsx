ff0c167960cf595d3e84e6ed7d21e6d0c2d58435
Ayush Saha <160033408+skyeyeye@users.noreply.github.com>
2025-10-07 09:57:00 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className="fixed bottom-2 right-4 text-sm text-gray-600 z-50">
-      Made with <span className="text-red-500">❤️</span> by <strong>Neha Singhal</strong>
+      Made with <span className="text-red-500">❤️</span> by <strong>Ayush Saha</strong>
     </div>
   );
 };
